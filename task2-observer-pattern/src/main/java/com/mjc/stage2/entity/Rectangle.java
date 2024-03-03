f0205145519d3ea2a3d69d8941d352350a077aaa
murod <murad.djanikulov@gmail.com>
2024-03-03 10:47:28 +0500

    word parser done

--- task2-observer-pattern/src/main/java/com/mjc/stage2/entity/Rectangle.java
+++ task2-observer-pattern/src/main/java/com/mjc/stage2/entity/Rectangle.java
@@ -1,15 +1,23 @@
 package com.mjc.stage2.entity;
 
-public class Rectangle {
+import com.mjc.stage2.Observable;
+import com.mjc.stage2.Observer;
+import com.mjc.stage2.event.RectangleEvent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rectangle implements Observable {
     private int id;
     private double sideA;
     private double sideB;
+    private List<Observer> observers;
 
-    // Write your code here!
     public Rectangle(int id, double sideA, double sideB) {
         this.id = id;
         this.sideA = sideA;
         this.sideB = sideB;
+        this.observers = new ArrayList<>();
     }
 
     public int getId() {
@@ -18,6 +26,7 @@ public class Rectangle {
 
     public void setId(int id) {
         this.id = id;
+        notifyObserver();
     }
 
     public double getSideA() {
@@ -26,7 +35,7 @@ public class Rectangle {
 
     public void setSideA(double sideA) {
         this.sideA = sideA;
-        // Write your code here!
+        notifyObserver();
     }
 
     public double getSideB() {
@@ -35,7 +44,22 @@ public class Rectangle {
 
     public void setSideB(double sideB) {
         this.sideB = sideB;
-        // Write your code here!
+        notifyObserver();
     }
-// Write your code here!
+
+    @Override
+    public void addObserver(Observer o) {
+        observers.add(o);
+    }
+
+    @Override
+    public void removeObserver(Observer o) {
+        observers.remove(o);
+    }
+
+    @Override
+    public void notifyObserver() {
+        RectangleEvent event = new RectangleEvent(this);
+        observers.forEach(observer -> observer.handleEvent(event));
+    }
 }
